6f7e8d7c10de38ed6bc2a2b7dfde229f9e33798f
Harshvardhan Parihar <harshvardhanparihar@gmail.com>
2019-01-06 14:10:42 +0530

    Added ml programs

--- machine_learning/doc.rb
+++ machine_learning/doc.rb
@@ -12,3 +12,10 @@ pip install numpy
 links
 https://www.datacamp.com/community/blog/scikit-learn-cheat-sheet
 https://startupsventurecapital.com/essential-cheat-sheets-for-machine-learning-and-deep-learning-researchers-efb6a8ebd2e5
+=======
+pip install matplotlib
+
+
+
+Differentaion
+https://www.slideshare.net/timschmitz/differentiation-427022
