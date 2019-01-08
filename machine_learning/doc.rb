36138e6b5e97015671315d8a6cb0e2fe4d89c244
Harshvardhan Parihar <harshvardhanparihar@gmail.com>
2019-01-08 17:38:45 +0530

    added links

--- machine_learning/doc.rb
+++ machine_learning/doc.rb
@@ -7,3 +7,8 @@ python
 import sklearn
 
 pip install numpy
+
+
+links
+https://www.datacamp.com/community/blog/scikit-learn-cheat-sheet
+https://startupsventurecapital.com/essential-cheat-sheets-for-machine-learning-and-deep-learning-researchers-efb6a8ebd2e5
